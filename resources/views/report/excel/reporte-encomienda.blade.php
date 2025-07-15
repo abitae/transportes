b88420f633b6b09a04cc472cd18b3e2d952f408a
Abel Arana <abel.arana@hotmail.com>
2025-07-15 12:35:14 -0500

    Actualización de la vista de reporte de encomiendas en Excel, añadiendo nuevas columnas para descuento, método de pago, tipo de comprobante, fecha de registro y fecha de entrega. Se mejoró la visualización de documentos de traslado y se optimizó la forma de mostrar los paquetes.

--- resources/views/report/excel/reporte-encomienda.blade.php
+++ resources/views/report/excel/reporte-encomienda.blade.php
@@ -9,37 +9,57 @@
             <th>CANTIDAD</th>
             <th>PAQUETES</th>
             <th>MONTO</th>
+            <th>DESCUENTO</th>
+            <th>METODO DE PAGO</th>
+            <th>TIPO DE COMPROBANTE</th>
             <th>RETORNO</th>
             <th>TIPO ENVIO</th>
             <th>CREDITO</th>
             <th>DOCUMENTO</th>
-            <th>ESTADO</th>
+            <th>FECHA DE REGISTRO</th>
+            <th>FECHA DE ENTRAGA</th>
         </tr>
     </thead>
     <tbody>
         @foreach($encomiendas as $encomiendaLibre)
                 <tr>
                     <td>{{ $encomiendaLibre->code }}</td>
-                    <td>{{ $encomiendaLibre->doc_traslado ?? 'S/D' }}</td>
-                    <td>{{ $encomiendaLibre->remitente->name }}</td>
-                    <td>{{ $encomiendaLibre->remitente->phone }}</td>
-                    <td>{{ $encomiendaLibre->destinatario->name }}</td>
+                    <td>
+                        @php
+                            $docs = $encomiendaLibre->docsTraslado ? json_decode($encomiendaLibre->docsTraslado, true) : null;
+                        @endphp
+                        @if($docs && is_array($docs) && count($docs) > 0)
+                            @foreach($docs as $doc)
+                                {{ is_array($doc) ? json_encode($doc) : $doc }}@if(!$loop->last), @endif
+                            @endforeach
+                        @else
+                            NINGUNO
+                        @endif
+                    </td>
+                    <td>{{ $encomiendaLibre->remitente->name ?? '' }}</td>
+                    <td>{{ $encomiendaLibre->remitente->phone ?? '' }}</td>
+                    <td>{{ $encomiendaLibre->destinatario->name ?? '' }}</td>
                     <td>{{ $encomiendaLibre->cantidad }}</td>
                     <td>
                         @php
-                            $packsLibre = '';
+                            $packsLibre = [];
+                            foreach ($encomiendaLibre->paquetes as $paquete) {
+                                $packsLibre[] = $paquete->description . '(' . $paquete->cantidad . ')(' . $paquete->amount . ')';
+                            }
                         @endphp
-                        @forelse ($encomiendaLibre->paquetes as $paquete)
-                            {{ $packsLibre . '' . $paquete->description . '(' . $paquete->cantidad . ')' . '(' . $paquete->amount . ')-' }}
-                        @empty
-                        @endforelse
+                        {{ implode(' - ', $packsLibre) }}
                     </td>
                     <td>{{ $encomiendaLibre->monto }}</td>
+                    <td>{{ $encomiendaLibre->monto_descuento }}</td>
                     <td>{{ $encomiendaLibre->isReturn ? 'SI' : 'NO' }}</td>
                     <td>{{ $encomiendaLibre->estado_pago}}</td>
                     <td>{{ $encomiendaLibre->tipo_pago }}</td>
                     <td>{{ $encomiendaLibre->tipo_comprobante }}</td>
-                    <td>{{ $encomiendaLibre->estado_cretido ?? 'incompleto' }}</td>
+                    <td>{{ $encomiendaLibre->metodo_pago ?? 'CONTADO' }}</td>
+                    <td>{{ $encomiendaLibre->tipo_comprobante ?? 'NINGUNO' }}</td>
+
+                    <td>{{ $encomiendaLibre->fecha_creacion }}</td>
+                    <td>{{ $encomiendaLibre->fecha_entrega }}</td>
                 </tr>
         @endforeach
     </tbody>
